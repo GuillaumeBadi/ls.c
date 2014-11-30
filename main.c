6568291efa7b393e6f3376f6cffbbf9c46a8cf99
Weshguillaume <weshguillaume@sd-52561.dedibox.fr>
2014-11-30 16:21:40 +0100

    ewgweg

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int					main(int ac, char **av)
 	int				flags;
 	t_node			*dir;
 
-	dir = new_elem(av[1], av[1]);
+	dir = new_elem(".", ".");
 	dir->first = 1;
 	flags = 0; //0b00000
 	raise_flags(ac, av, &flags);
